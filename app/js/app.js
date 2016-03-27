16d02a2e77995886ef959bbadfb32d0cbbde2538
N.Shimizu <chikoski@gmail.com>
2016-03-27 11:25:50 +0900

    ボリュームの変更をなめらかに行えるように変更

--- app/js/app.js
+++ app/js/app.js
@@ -23,10 +23,13 @@ function to100(){
   player.currentTime = player.currentTime + 10;
 }
 
-function changeVolume(){
-  var volumeSlider = document.querySelector("#volume");
-  var player = document.querySelector("audio");
-  player.volume = volumeSlider.value;
+function changeVolume(event){
+  if(event.buttons != 0){
+    var player = document.querySelector("audio");
+    var relativeX = event.clientX / event.target.clientWidth;
+    var volume = Math.min(relativeX, 1.0);
+    player.volume = volume;
+  }
 }
 
 function init(){
@@ -40,7 +43,7 @@ function init(){
   to100Button.addEventListener("click", to100);
   
   var volumeSlider = document.querySelector("#volume");
-  volumeSlider.addEventListener("change", changeVolume);
+  volumeSlider.addEventListener("mousemove", changeVolume);
 };
 
 window.addEventListener("load", event =>{
