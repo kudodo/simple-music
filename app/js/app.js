f92a30a1bde00ac9c4ddf128984fbb07bd118c44
N.Shimizu <chikoski@gmail.com>
2016-03-27 10:49:05 +0900

    初日の開発結果を反映

--- app/js/app.js
+++ app/js/app.js
@@ -8,9 +8,39 @@ function installServiceWorker(){
   });
 }
 
+function playMusic(){
+  console.log("再生ぼたんがおされたよ");
+  var player = document.querySelector("audio");
+  if(player.paused){
+    player.play();
+  }else{
+    player.pause();
+  }
+}
+
+function to100(){
+  var player = document.querySelector("audio");
+  player.currentTime = player.currentTime + 10;
+}
+
+function changeVolume(){
+  var volumeSlider = document.querySelector("#volume");
+  var player = document.querySelector("audio");
+  player.volume = volumeSlider.value;
+}
+
 function init(){
   var player = document.querySelector("audio");
   console.log(player);
+
+  var playButton = document.querySelector("#playButton");
+  playButton.addEventListener("click", playMusic);
+  
+  var to100Button = document.querySelector("#to100");
+  to100Button.addEventListener("click", to100);
+  
+  var volumeSlider = document.querySelector("#volume");
+  volumeSlider.addEventListener("change", changeVolume);
 };
 
 window.addEventListener("load", event =>{
